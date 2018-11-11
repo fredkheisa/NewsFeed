fd909d18bd2bbbbac0de019275814da49d03ca04
Github User Name <you@example.com>
2018-11-11 18:42:32 +0300

    innoitialised the app successfully

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+     # Getting popular headlines
+    biashara = get_news('business')
+    michezo = get_news('sports')
+
+    print(biashara)
+    print(michezo)
+
+    title = 'PATAPATA NEWS AND STORIES'
+    return render_template('index.html', title = title, biashara = biashara, michezo = michezo)
