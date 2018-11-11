a81a1f539d1ae82ec8bdc06bd3665efa53cb7d51
Github User Name <you@example.com>
2018-11-11 19:13:55 +0300

    created the tests for news

---
+++ app/models/news.py
@@ -0,0 +1,15 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self, source, author, title, description, url, image_url, publish_time):
+
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image_url = image_url
+        self.publish_time = publish_time
+    
