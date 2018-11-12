c2e568c5a7435f7959ed6d38a3904c780d732f24
Github User Name <you@example.com>
2018-11-12 12:25:57 +0300

    innitiated the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+app = Flask(__name__)
 
 from app import views
-from app import error
