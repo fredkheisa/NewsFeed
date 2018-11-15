6bbc950557b3f782e3e6f6cbce393ae18caa03a1
Github User Name <you@example.com>
2018-11-15 14:34:02 +0300

    registering the blue print

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
+def create_app(config_name):
+    
+    # app = Flask(__name__)
+    app = Flask(__name__,instance_relative_config = True)
+    app.config.from_object(config_options[config_name])
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+    # Initializing Flask Extensions
+    bootstrap = Bootstrap(app)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
 
-from app import views
+    return app
--- app/models.py
+++ app/models.py
@@ -1,14 +1,26 @@
+# source class
 class Source:
     '''
-    News class to define News Objects
+    Source class to define Objects
     '''
 
-    def __init__(self, source, author, title, description, url, image_url, publish_time):
+    def __init__(self,id,name,description):
+        self.id =id
+        self.name = name
+        self.description = description
+         
+# Article class
+class Article:
+    '''
+    Article class to define Objects
+    '''
 
-        self.source = source
-        self.author = author
+    def __init__(self,id,name,description,title,author,url,urlToImage,publishedAt):
+        self.id =id
+        self.name = name
+        self.author= author
         self.title = title
         self.description = description
-        self.url = url
-        self.image_url = image_url
-        self.publish_time = publish_time
+        self.url = url 
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
