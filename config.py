3f5e2ef9469bd9cc015b70a54c89c437883238dd
Github User Name <you@example.com>
2018-11-15 14:27:44 +0300

    editing the folder stucture

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    SOURCE_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&language=en&apiKey={}'
-
-
+    
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    SOURCES_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
 class ProdConfig(Config):
     '''
     Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-   
+    pass
+
 
 class DevConfig(Config):
     '''
@@ -20,5 +24,9 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
+    DEBUG = True    
 
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app
-from app.models import app
 from flask_script import Manager,Server
+
 app = create_app('development')
 
-
-manager = Manager(app)
+manager =Manager(app)
 manager.add_command('server',Server)
-if __name__ == '__main__':
-    manager.run()
+
+if __name__ == "__main__":
+  manager.run()
